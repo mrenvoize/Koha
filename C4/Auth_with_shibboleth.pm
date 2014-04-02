7be6bd2339a855b25dea526112cb478583cabf6a
Jesse Weaver <pianohacker@gmail.com>
2014-04-02 15:53:03 +0000

    BUG12026: Adds auto-provisioning to shibboleth
    - Adds the option to map shibboleth attributes to koha user columns
      and allows mapping to auto create non-existant users
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>

--- C4/Auth_with_shibboleth.pm
+++ C4/Auth_with_shibboleth.pm
@@ -22,6 +22,8 @@ use warnings;
 
 use C4::Debug;
 use C4::Context;
+use C4::Members qw( AddMember_Auto );
+use C4::Members::Messaging;
 use Carp;
 use CGI;
 
@@ -104,9 +106,13 @@ sub checkpw_shib {
         return ( 1, $retnumber, $userid );
     }
 
-    # If we reach this point, the user is not a valid koha user
-    $debug and warn "User with $config->{matchpoint} of $match is not a valid Koha user";
-    return 0;
+    if ( $config->{'autocreate'} ) {
+        return _autocreate( $dbh, $config, $match );
+    } else {
+        # If we reach this point, the user is not a valid koha user
+        $debug and warn "User with $config->{matchpoint} of $match is not a valid Koha user";
+        return 0;
+    }
 }
 
 sub _get_uri {
@@ -144,6 +150,21 @@ sub _get_shib_config {
         }
         return 0;
     }
+}
+
+sub _autocreate {
+    my ( $dbh, $config, $match ) = @_;
+
+    my %borrower = ( $config->{matchpoint} => $match );
+
+    while ( my ( $key, $entry ) = each %{$config->{'mapping'}} ) {
+        $borrower{$key} = ( $entry->{'is'} && $ENV{ $entry->{'is'} } ) || $entry->{'content'} || '';
+    }
+
+    %borrower = AddMember_Auto( %borrower );
+    C4::Members::Messaging::SetMessagingPreferencesFromDefaults( { borrowernumber => $borrower{'borrowernumber'}, categorycode => $borrower{'categorycode'} } );
+
+    return ( 1, $borrower{'cardnumber'}, $borrower{'userid'} );
 }
 
 1;
