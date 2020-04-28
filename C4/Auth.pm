33198f2777f2bd30a4a0ebbe4f51e25e1cb86e25
Nicolas Legrand <nicolas.legrand@bulac.fr>
2020-04-28 12:02:15 +0200

    Bug 24201: (follow-up) add desk choice with library choice
    You should be able to add desk choice when you are logging in or
    changing library.
    
    Test plan:
    
    1. apply patch
    2. have at least three libraries, one without desk, one with one and
    one with a few.
    3. At login, when choosing a library, it should enable all desks it
    has. Pick one.
    4. the desk id and name should be set in your session and appear in
    the top right, next to the library name.
    5. change library and desks from intranet (at the set-library.pl page)
    6. you should have the same behaviours
    7. if you have a library without a desk, it should prompt you a '---'
    option and no desks will be attached to the session.

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1070,7 +1070,7 @@ sub checkauth {
                     C4::Context->_unset_userenv($sessionID);
                 }
                 my ( $borrowernumber, $firstname, $surname, $userflags,
-                    $branchcode, $branchname, $emailaddress );
+                    $branchcode, $branchname, $emailaddress, $desk_id, $desk_name );
 
                 if ( $return == 1 ) {
                     my $select = "
@@ -1114,6 +1114,11 @@ sub checkauth {
                         my $library = Koha::Libraries->find($branchcode);
                         $branchname = $library? $library->branchname: '';
                     }
+                    if ( $query->param('desk_id') ) {
+                        $desk_id = $query->param('desk_id');
+                        my $desk = Koha::Desks->find($desk_id);
+                        $desk_name = $desk ? $desk->desk_name : '';
+                    }
                     my $branches = { map { $_->branchcode => $_->unblessed } Koha::Libraries->search };
                     if ( $type ne 'opac' and C4::Context->boolean_preference('AutoLocation') ) {
 
@@ -1149,6 +1154,8 @@ sub checkauth {
                     $session->param( 'surname',      $surname );
                     $session->param( 'branch',       $branchcode );
                     $session->param( 'branchname',   $branchname );
+                    $session->param( 'desk_id',      $desk_id);
+                    $session->param( 'desk_name',     $desk_name);
                     $session->param( 'flags',        $userflags );
                     $session->param( 'emailaddress', $emailaddress );
                     $session->param( 'ip',           $session->remote_addr() );
@@ -1164,7 +1171,7 @@ sub checkauth {
                     $session->param('surname'),      $session->param('branch'),
                     $session->param('branchname'),   $session->param('flags'),
                     $session->param('emailaddress'), $session->param('shibboleth'),
-                    $session->param('desk_id'),      $session->param('desk_name')
+                    $session->param('desk_id'), $session->param('desk_name')
                 );
 
             }
