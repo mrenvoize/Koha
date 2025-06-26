3042f06d63b824530e928c67c6999a86f75d4598
Chloe Zermatten <chloe.zermatten@openfifth.co.uk>
2025-06-26 08:32:08 +0000

    WIP: Koha Checkout GetOverduesBy

--- Koha/Checkouts.pm
+++ Koha/Checkouts.pm
@@ -123,6 +123,116 @@ sub object_class {
     return 'Koha::Checkout';
 }
 
+=head3 GetOverduesBy
+
+my $overdues = Koha::Checkouts::GetOverdues( $parameters )
+
+Fetches all overdues, and optionally filters by
+- patron OR patron category  AND/OR  
+- item home OR issue branch
+
+=cut
+
+sub GetOverduesBy {
+    my ($parameters) = shift;
+
+    # my $borrowernumber = $parameters->{'borrowernumber'} // '';
+    # warn $borrowernumber;
+
+    my %attributes = (
+
+        # alternative to 'join' to be considered - would load much more data though, most of it likely being unecessary
+        # prefetch  => {
+        #     'patron' => 'category',
+        #     'item' => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
+        # },
+        join => {
+            'patron' => 'category',
+            'item'   => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
+        },
+        '+select' => [
+            'borrowernumber',
+            'patron.firstname',
+            'patron.surname',
+            'patron.address',
+            'patron.address2',
+            'patron.city',
+            'patron.zipcode',
+            'patron.country',
+            'patron.email',
+            'patron.emailpro',
+            'patron.B_email',
+            'patron.smsalertnumber',
+            'patron.phone',
+            'patron.cardnumber',
+            'biblioitem.itemtype',
+            'homebranch.branchname',
+            'category.overduenoticerequired',
+        ],
+        '+as' => [
+            'borrowernumber',
+            'patron_firstname',
+            'patron_surname',
+            'patron_address',
+            'patron_address2',
+            'patron_city',
+            'patron_zipcode',
+            'patron_country',
+            'patron_email',
+            'patron_emailpro',
+            'patron_B_email',
+            'patron_smsalertnumber',
+            'patron_phone',
+            'patron_cardnumber',
+            'biblioitem_itemtype',
+            'homebranch_branchname',
+            'category_overduenoticerequired',
+        ],
+    );
+
+    # # FILTERS:
+
+    my %conditions;
+
+    # patron (borrowernumber) or patron categorycode
+    if ( defined $parameters->{'borrowernumber'} ) {
+        $conditions{'me.borrowernumber'} = $parameters->{'borrowernumber'};
+    } elsif ( defined $parameters->{'patron_categorycode'} ) {
+        $conditions{'patron.categorycode'} = $parameters->{'patron_categorycode'};
+    }
+
+    # owning or issue branch
+    if ( defined $parameters->{'item_homebranch'} ) {
+        $conditions{'item.homebranch'} = $parameters->{'item_homebranch'};
+    } elsif ( defined $parameters->{'item_issuebranch'} ) {
+        $conditions{'issue.branchcode'} = $parameters->{'item_issuebranch'};
+    }
+
+    # item type, either at bib or item level
+    my $itemtypes = join( ", ", Koha::ItemTypes->search()->get_column('itemtype') );
+    if ( C4::Context->preference('item-level_itypes') ) {
+        $conditions{'item.itype'} = { '-in', $itemtypes };
+    } else {
+        $conditions{'biblioitem.itemtype'} = { '-in', $itemtypes };
+    }
+
+    my $checkouts_set = Koha::Checkouts->new();
+
+    try {
+        my $search_rs = $checkouts_set->search( \%conditions, \%attributes );
+        my @results;
+
+        # may want to leave this iteration in the script so we don't iterate twice
+        while ( my $row = $search_rs->next() ) {
+            push( @results, $row );
+        }
+        return @results;
+
+    } catch {
+        $checkouts_set->unhandled_exception($_);
+    };
+}
+
 =head1 AUTHOR
 
 Kyle M Hall <kyle@bywatersolutions.com>
@@ -130,3 +240,89 @@ Kyle M Hall <kyle@bywatersolutions.com>
 =cut
 
 1;
+
+# GetOverdues - alternative SQL version
+# sub GetOverdues {
+#     my ( $patron_categorycode, $item_homebranch, $item_issuebranch ) = @_;
+
+#     my $dbh = C4::Context->dbh();
+
+#     my $borrower_sql = <<"END_SQL";
+# SELECT
+#     issues.borrowernumber,
+#     borrowers.firstname,
+#     borrowers.surname,
+#     borrowers.address,
+#     borrowers.address2,
+#     borrowers.city,
+#     borrowers.zipcode,
+#     borrowers.country,
+#     borrowers.email,
+#     borrowers.emailpro,
+#     borrowers.B_email,
+#     borrowers.smsalertnumber,
+#     borrowers.phone,
+#     borrowers.cardnumber,
+#     borrowers.categorycode,
+#     biblio.*,
+#     biblioitems.itemtype AS b_itemtype,
+#     items.*,
+#     issues.*,
+#     branches.branchname,
+#     categories.overduenoticerequired
+# FROM
+#     issues
+# JOIN
+#     borrowers ON issues.borrowernumber = borrowers.borrowernumber
+# JOIN
+#     categories ON borrowers.categorycode = categories.categorycode
+# JOIN
+#     items ON issues.itemnumber = items.itemnumber
+# JOIN
+#     biblio ON biblio.biblionumber = items.biblionumber
+# JOIN
+#     biblioitems ON biblio.biblionumber = biblioitems.biblionumber
+# JOIN
+#     branches ON branches.branchcode = items.homebranch
+# WHERE
+#     items.itemlost = 0
+# END_SQL
+
+#     # AND TO_DAYS($date)-TO_DAYS(issues.date_due) >= 0
+#     my @borrower_parameters;
+#     if ($item_homebranch) {
+#         $borrower_sql .= ' AND items.homebranch=? ';
+#         push @borrower_parameters, $item_homebranch;
+#     } elsif ($item_issuebranch) {
+#         $borrower_sql .= ' AND issues.branchcode=? ';
+#         push @borrower_parameters, $item_issuebranch;
+#     }
+#     if ($patron_categorycode) {
+#         $borrower_sql .= ' AND borrowers.categorycode=? ';
+#         push @borrower_parameters, $patron_categorycode;
+#     }
+
+#     my @itemtypes = Koha::ItemTypes->search()->get_column('itemtype');
+
+#     if (@itemtypes) {
+#         my $placeholders = join( ", ", ("?") x @itemtypes );
+#         if (C4::Context->preference('item-level_itypes')) {
+#             $borrower_sql .= " AND items.itype IN ($placeholders) ";
+#         } else {
+#             $borrower_sql .= " AND biblioitems.itemtype IN ($placeholders) ";
+#         }
+#         push @borrower_parameters, @itemtypes;
+#     }
+#     $borrower_sql .= '  ORDER BY issues.borrowernumber';
+
+#     # $sth gets borrower info if at least one overdue item has triggered the overdue action.
+#     my $sth = $dbh->prepare($borrower_sql);
+#     $sth->execute(@borrower_parameters);
+
+#     # leaving this in the script prevented duplicating iterations..
+#     my @results;
+#     while ( my $row = $sth->fetchrow_hashref ) {
+#         push( @results, $row );
+#     }
+#     return \@results;
+# }
