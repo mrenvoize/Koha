5d89b16214e07253eb2ecb3b6fffd1d23362fc7d
Chloe Zermatten <chloe.zermatten@openfifth.co.uk>
2025-07-01 11:16:33 +0000

    WIP: refactor: build custom SQL query
    haven't find a way to pass the TO_DAYS($date)-TO_DAYS(issues.date_due) >= 0
    condition to the DIB search() method
    -> swapping to the custom SQL stmt used in overdue_notices.pl

--- Koha/Checkouts.pm
+++ Koha/Checkouts.pm
@@ -133,107 +133,116 @@ Fetches all overdues, and optionally filters by
 
 =cut
 
-sub GetOverduesBy {
-    my ($parameters) = shift;
+# sub GetOverduesBy {
+#     my ($parameters) = shift;
 
-    my %attributes;
+#     my %attributes;
 
-    # if ( $parameters->{get_summary} != 1 ) {
-    #     warn 'ran prefetch';
-    #     $attributes{prefetch} = {
-    #         'patron' => 'category',
-    #         'item'   => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
-    #     };
-    # } else {
-    $attributes{join} = {
-        'patron' => 'category',
-        'item'   => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
-    };
+#     # if ( $parameters->{get_summary} != 1 ) {
+#     #     warn 'ran prefetch';
+#     #     $attributes{prefetch} = {
+#     #         'patron' => 'category',
+#     #         'item'   => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
+#     #     };
+#     # } else {
+#     $attributes{join} = {
+#         'patron' => 'category',
+#         'item'   => [ 'homebranch', { 'biblio' => 'biblioitem' } ]
+#     };
 
-    # $attributes{'+select'} = [
-    #     'borrowernumber',
-    #     'patron.firstname',
-    #     'patron.surname',
-    #     'patron.address',
-    #     'patron.address2',
-    #     'patron.city',
-    #     'patron.zipcode',
-    #     'patron.country',
-    #     'patron.email',
-    #     'patron.emailpro',
-    #     'patron.B_email',
-    #     'patron.smsalertnumber',
-    #     'patron.phone',
-    #     'patron.cardnumber',
-    #     'biblioitem.itemtype',
-    #     'homebranch.branchname',
-    #     'category.overduenoticerequired',
-    #     'item.homebranch',
-    # ];
-    # $attributes{'+as'} = [
-    #     'borrowernumber',
-    #     'patron_firstname',
-    #     'patron_surname',
-    #     'patron_address',
-    #     'patron_address2',
-    #     'patron_city',
-    #     'patron_zipcode',
-    #     'patron_country',
-    #     'patron_email',
-    #     'patron_emailpro',
-    #     'patron_B_email',
-    #     'patron_smsalertnumber',
-    #     'patron_phone',
-    #     'patron_cardnumber',
-    #     'biblioitem_itemtype',
-    #     'homebranch_branchname',
-    #     'category_overduenoticerequired',
-    #     'item_homebranch',
-    # ];
-    # }
+#     # $attributes{'+select'} = [
+#     #     'borrowernumber',
+#     #     'patron.firstname',
+#     #     'patron.surname',
+#     #     'patron.address',
+#     #     'patron.address2',
+#     #     'patron.city',
+#     #     'patron.zipcode',
+#     #     'patron.country',
+#     #     'patron.email',
+#     #     'patron.emailpro',
+#     #     'patron.B_email',
+#     #     'patron.smsalertnumber',
+#     #     'patron.phone',
+#     #     'patron.cardnumber',
+#     #     'biblioitem.itemtype',
+#     #     'homebranch.branchname',
+#     #     'category.overduenoticerequired',
+#     #     'item.homebranch',
+#     # ];
+#     # $attributes{'+as'} = [
+#     #     'borrowernumber',
+#     #     'patron_firstname',
+#     #     'patron_surname',
+#     #     'patron_address',
+#     #     'patron_address2',
+#     #     'patron_city',
+#     #     'patron_zipcode',
+#     #     'patron_country',
+#     #     'patron_email',
+#     #     'patron_emailpro',
+#     #     'patron_B_email',
+#     #     'patron_smsalertnumber',
+#     #     'patron_phone',
+#     #     'patron_cardnumber',
+#     #     'biblioitem_itemtype',
+#     #     'homebranch_branchname',
+#     #     'category_overduenoticerequired',
+#     #     'item_homebranch',
+#     # ];
+#     # }
 
-    # FILTERS:
+#     # FILTERS:
 
-    my %conditions;
+#     my %conditions;
 
-    # patron (borrowernumber) or patron categorycode
-    if ( defined $parameters->{'borrowernumber'} ) {
-        $conditions{'me.borrowernumber'} = $parameters->{'borrowernumber'};
-    } elsif ( defined $parameters->{'patron_categorycode'} ) {
-        $conditions{'patron.categorycode'} = $parameters->{'patron_categorycode'};
-    }
+#     # is overdue
+#     # FIXME: can be written as my $date = $parameters->{'date'} ?? dt_from_string() instead?
+#     my $date = $parameters->{'date'} ? $parameters->{'date'} : dt_from_string();
+#     $conditions{'item.itemlost'} = 0;
+#     # FIXME: cannot seem to pass the below SQL functions as a condition to the DBI search() method
+#     # -> impossible? -> moving back to using SQL instead
+#     $conditions{"TO_DAYS($date)-TO_DAYS(issues.date_due)"} = { ">=", 0};
 
-    # owning or issue branch
-    if ( defined $parameters->{'item_homebranch'} ) {
-        $conditions{'item.homebranch'} = $parameters->{'item_homebranch'};
-    } elsif ( defined $parameters->{'item_issuebranch'} ) {
-        $conditions{'issue.branchcode'} = $parameters->{'item_issuebranch'};
-    }
+#     # patron (borrowernumber) or patron categorycode
+#     if ( defined $parameters->{'borrowernumber'} ) {
+#         $conditions{'me.borrowernumber'} = $parameters->{'borrowernumber'};
+#     } elsif ( defined $parameters->{'patron_categorycode'} ) {
+#         $conditions{'patron.categorycode'} = $parameters->{'patron_categorycode'};
+#     }
 
-    # item type, either at bib or item level
-    my $itemtypes = join( ", ", Koha::ItemTypes->search()->get_column('itemtype') );
-    if ( C4::Context->preference('item-level_itypes') ) {
-        $conditions{'item.itype'} = { '-in', $itemtypes };
-    } else {
-        $conditions{'biblioitem.itemtype'} = { '-in', $itemtypes };
-    }
+#     # owning or issue branch
+#     if ( defined $parameters->{'item_homebranch'} ) {
+#         $conditions{'item.homebranch'} = $parameters->{'item_homebranch'};
+#     } elsif ( defined $parameters->{'item_issuebranch'} ) {
+#         $conditions{'issue.branchcode'} = $parameters->{'item_issuebranch'};
+#     }
 
-    my $checkouts_set = Koha::Checkouts->new();
+#     # item type, either at bib or item level
+#     # my $itemtypes = join( ", ", Koha::ItemTypes->search()->get_column('itemtype') );
+#     # if ( C4::Context->preference('item-level_itypes') ) {
+#         # $conditions{'item.itype'} = { '-in', $itemtypes };
+#     # } else {
+#     #     $conditions{'biblioitem.itemtype'} = { '-in', $itemtypes };
+#     # }
 
-    try {
-        my $search_rs = $checkouts_set->search( \%conditions, \%attributes );
-        my @results;
+#     my $checkouts_set = Koha::Checkouts->new();
 
-        # may want to leave this iteration in the script so we don't iterate twice
-        while ( my $row = $search_rs->next() ) {
-            push( @results, $row );
-        }
-        return @results;
+#     try {
+#         # FIXME: does search
+#         my $search_rs = $checkouts_set->search( \%conditions, \%attributes );
+#         my @results;
 
-    } catch {
-        $checkouts_set->unhandled_exception($_);
-    };
-}
+#         # may want to leave this iteration in the script so we don't iterate twice
+#         while ( my $row = $search_rs->next() ) {
+#             push( @results, $row );
+#         }
+#         return @results;
+
+#     } catch {
+#         $checkouts_set->unhandled_exception($_);
+#     };
+# }
 
 =head1 AUTHOR
 
@@ -244,87 +253,96 @@ Kyle M Hall <kyle@bywatersolutions.com>
 1;
 
 # GetOverdues - alternative SQL version
-# sub GetOverdues {
-#     my ( $patron_categorycode, $item_homebranch, $item_issuebranch ) = @_;
+sub GetOverduesBy {
+    my ($parameters) = shift;
 
-#     my $dbh = C4::Context->dbh();
+    my $dbh  = C4::Context->dbh();
+    my $date = $parameters->{'date'} ? $parameters->{'date'} : 'NOW()';
 
-#     my $borrower_sql = <<"END_SQL";
-# SELECT
-#     issues.borrowernumber,
-#     borrowers.firstname,
-#     borrowers.surname,
-#     borrowers.address,
-#     borrowers.address2,
-#     borrowers.city,
-#     borrowers.zipcode,
-#     borrowers.country,
-#     borrowers.email,
-#     borrowers.emailpro,
-#     borrowers.B_email,
-#     borrowers.smsalertnumber,
-#     borrowers.phone,
-#     borrowers.cardnumber,
-#     borrowers.categorycode,
-#     biblio.*,
-#     biblioitems.itemtype AS b_itemtype,
-#     items.*,
-#     issues.*,
-#     branches.branchname,
-#     categories.overduenoticerequired
-# FROM
-#     issues
-# JOIN
-#     borrowers ON issues.borrowernumber = borrowers.borrowernumber
-# JOIN
-#     categories ON borrowers.categorycode = categories.categorycode
-# JOIN
-#     items ON issues.itemnumber = items.itemnumber
-# JOIN
-#     biblio ON biblio.biblionumber = items.biblionumber
-# JOIN
-#     biblioitems ON biblio.biblionumber = biblioitems.biblionumber
-# JOIN
-#     branches ON branches.branchcode = items.homebranch
-# WHERE
-#     items.itemlost = 0
-# END_SQL
+    my $borrower_sql = <<"END_SQL";
+SELECT
+    issues.borrowernumber,
+    borrowers.firstname,
+    borrowers.surname,
+    borrowers.address,
+    borrowers.address2,
+    borrowers.city,
+    borrowers.zipcode,
+    borrowers.country,
+    borrowers.email,
+    borrowers.emailpro,
+    borrowers.B_email,
+    borrowers.smsalertnumber,
+    borrowers.phone,
+    borrowers.cardnumber,
+    borrowers.categorycode,
+    biblio.*,
+    biblioitems.itemtype AS b_itemtype,
+    items.*,
+    issues.*,
+    branches.branchname,
+    categories.overduenoticerequired
+FROM
+    issues
+JOIN
+    borrowers ON issues.borrowernumber = borrowers.borrowernumber
+JOIN
+    categories ON borrowers.categorycode = categories.categorycode
+JOIN
+    items ON issues.itemnumber = items.itemnumber
+JOIN
+    biblio ON biblio.biblionumber = items.biblionumber
+JOIN
+    biblioitems ON biblio.biblionumber = biblioitems.biblionumber
+JOIN
+    branches ON branches.branchcode = items.homebranch
+WHERE
+    items.itemlost = 0
+    AND TO_DAYS($date)-TO_DAYS(issues.date_due) >= 0
+END_SQL
 
-#     # AND TO_DAYS($date)-TO_DAYS(issues.date_due) >= 0
-#     my @borrower_parameters;
-#     if ($item_homebranch) {
-#         $borrower_sql .= ' AND items.homebranch=? ';
-#         push @borrower_parameters, $item_homebranch;
-#     } elsif ($item_issuebranch) {
-#         $borrower_sql .= ' AND issues.branchcode=? ';
-#         push @borrower_parameters, $item_issuebranch;
-#     }
-#     if ($patron_categorycode) {
-#         $borrower_sql .= ' AND borrowers.categorycode=? ';
-#         push @borrower_parameters, $patron_categorycode;
-#     }
+    # conditions
 
-#     my @itemtypes = Koha::ItemTypes->search()->get_column('itemtype');
+    # owning or issue branch
+    my @borrower_parameters;
+    if ( $parameters->{'item_homebranch'} ) {
+        $borrower_sql .= ' AND items.homebranch=? ';
+        push @borrower_parameters, $parameters->{'item_homebranch'};
+    } elsif ( $parameters->{'item_issuebranch'} ) {
+        $borrower_sql .= ' AND issues.branchcode=? ';
+        push @borrower_parameters, $parameters->{'item_issuebranch'};
+    }
 
-#     if (@itemtypes) {
-#         my $placeholders = join( ", ", ("?") x @itemtypes );
-#         if (C4::Context->preference('item-level_itypes')) {
-#             $borrower_sql .= " AND items.itype IN ($placeholders) ";
-#         } else {
-#             $borrower_sql .= " AND biblioitems.itemtype IN ($placeholders) ";
-#         }
-#         push @borrower_parameters, @itemtypes;
-#     }
-#     $borrower_sql .= '  ORDER BY issues.borrowernumber';
+    # patron (borrowernumber) or patron categorycode
+    if ( defined $parameters->{'borrowernumber'} ) {
+        $borrower_sql .= ' AND borrowers.categorycode=? ';
+        push @borrower_parameters, $parameters->{'patron_categorycode'};
+    } elsif ( $parameters->{'patron_categorycode'} ) {
+        $borrower_sql .= ' AND borrowers.categorycode=? ';
+        push @borrower_parameters, $parameters->{'patron_categorycode'};
+    }
 
-#     # $sth gets borrower info if at least one overdue item has triggered the overdue action.
-#     my $sth = $dbh->prepare($borrower_sql);
-#     $sth->execute(@borrower_parameters);
+    # item type, either at bib or item level
+    my @itemtypes = Koha::ItemTypes->search()->get_column('itemtype');
+    if (@itemtypes) {
+        my $placeholders = join( ", ", ("?") x @itemtypes );
+        if ( C4::Context->preference('item-level_itypes') ) {
+            $borrower_sql .= " AND items.itype IN ($placeholders) ";
+        } else {
+            $borrower_sql .= " AND biblioitems.itemtype IN ($placeholders) ";
+        }
+        push @borrower_parameters, @itemtypes;
+    }
+    $borrower_sql .= '  ORDER BY issues.borrowernumber';
 
-#     # leaving this in the script prevented duplicating iterations..
-#     my @results;
-#     while ( my $row = $sth->fetchrow_hashref ) {
-#         push( @results, $row );
-#     }
-#     return \@results;
-# }
+    # $sth gets borrower info if at least one overdue item has triggered the overdue action.
+    my $sth = $dbh->prepare($borrower_sql);
+    $sth->execute(@borrower_parameters);
+
+    # leaving this in the script prevented duplicating iterations..
+    my @results;
+    while ( my $row = $sth->fetchrow_hashref ) {
+        push( @results, $row );
+    }
+    return @results;
+}
